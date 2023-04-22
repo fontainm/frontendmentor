493fd15f118ec6fa3bf7a149601b82d763c72a8a
Mathias <mathias.fontain@vol.at>
2023-04-22 15:37:56 +0200

    add form validation

--- age-calculator/src/components/AgeCalculator.vue
+++ age-calculator/src/components/AgeCalculator.vue
@@ -1,29 +1,100 @@
 <template>
   <div class="ac">
-    <div class="ac__input"></div>
-    <div class="ac__submit">
-      <div class="ac__submit--line"></div>
-      <button class="ac__submit--button">
-        <img src="icon-arrow.svg" />
-      </button>
-    </div>
+    <ValidationObserver v-slot="{ handleSubmit }">
+      <form @submit.prevent="handleSubmit(calculateAge)">
+        <div class="ac__input">
+          <ValidationProvider
+            class="ac__input--field"
+            rules="required|date:day"
+            v-slot="{ errors }"
+          >
+            <label for="day">Day</label>
+            <input name="day" v-model="day" placeholder="DD" type="number" />
+            <span>{{ errors[0] }}</span>
+          </ValidationProvider>
+          <ValidationProvider
+            class="ac__input--field"
+            rules="required|date:month"
+            v-slot="{ errors }"
+          >
+            <label for="month">Month</label>
+            <input
+              name="month"
+              v-model="month"
+              placeholder="MM"
+              type="number"
+            />
+            <span>{{ errors[0] }}</span>
+          </ValidationProvider>
+          <ValidationProvider
+            rules="required|date:year"
+            class="ac__input--field"
+            v-slot="{ errors }"
+          >
+            <label for="year">Year</label>
+            <input
+              name="year"
+              v-model="year"
+              placeholder="YYYY"
+              type="number"
+            />
+            <span>{{ errors[0] }}</span>
+          </ValidationProvider>
+        </div>
+        <div class="ac__submit">
+          <div class="ac__submit--line"></div>
+          <button type="submit" class="ac__submit--button">
+            <img src="icon-arrow.svg" />
+          </button>
+        </div>
+      </form>
+    </ValidationObserver>
     <div class="ac__output">
-      <div><span>--</span> years</div>
-      <div><span>--</span> months</div>
-      <div><span>--</span> days</div>
+      <div>
+        <span>{{ result.years }}</span> years
+      </div>
+      <div>
+        <span>{{ result.months }}</span> months
+      </div>
+      <div>
+        <span>{{ result.days }}</span> days
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
   name: "AgeCalculator",
 
   data() {
     return {
-      firstName: "",
-      lastName: "",
+      day: "",
+      month: "",
+      year: "",
+      result: {
+        years: "--",
+        months: "--",
+        days: "--",
+      },
     };
+  },
+
+  methods: {
+    calculateAge() {
+      let birthDate = moment({
+        year: this.year,
+        month: this.month - 1,
+        day: this.day,
+      });
+      let difference = moment().diff(birthDate, "ms");
+      let duration = moment.duration(difference);
+      this.result.years = duration.years();
+      this.result.months = duration.months();
+      this.result.days = duration.days();
+    },
   },
 };
 </script>
@@ -31,9 +102,47 @@ export default {
 <style scoped lang="scss">
 .ac {
   background: white;
-  border-radius: 30px 30px 60px 30px;
+  border-radius: 20px 20px 120px 20px;
   padding: 2rem;
   position: relative;
+  min-width: 480px;
+
+  &__input {
+    display: flex;
+
+    &--field {
+      display: flex;
+      flex-direction: column;
+      margin-right: 12px;
+      width: 120px;
+
+      label {
+        text-transform: uppercase;
+        font-weight: 700;
+        font-size: 12px;
+        color: var(--color-smoke-grey);
+      }
+
+      input {
+        border: 1px solid var(--color-light-grey);
+        border-radius: 4px;
+        font-size: 20px;
+        padding: 10px;
+        font-weight: 700;
+        margin: 6px 0;
+
+        &::placeholder {
+          color: var(--color-light-grey);
+        }
+      }
+
+      span {
+        color: var(--color-danger);
+        font-size: 10px;
+        font-style: italic;
+      }
+    }
+  }
 
   &__submit {
     display: flex;
@@ -43,7 +152,7 @@ export default {
     &--line {
       height: 1px;
       width: calc(100% - 70px);
-      background: hsl(0, 0%, 86%);
+      background: var(--color-light-grey);
     }
 
     &--button {
@@ -59,13 +168,13 @@ export default {
       cursor: pointer;
 
       &:hover {
-        background: black;
+        background: var(--color-black);
       }
     }
   }
 
   &__output {
-    font-size: 52px;
+    font-size: 60px;
     font-weight: 800;
     font-style: italic;
 
@@ -73,5 +182,17 @@ export default {
       color: var(--color-primary);
     }
   }
+}
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type="number"] {
+  -moz-appearance: textfield;
 }
 </style>
--- age-calculator/src/main.js
+++ age-calculator/src/main.js
@@ -1,9 +1,49 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './assets/styles.scss';
+import Vue from "vue";
+import App from "./App.vue";
+import "./assets/styles.scss";
+import {
+  ValidationObserver,
+  ValidationProvider,
+  extend,
+  setInteractionMode,
+} from "vee-validate";
+import { required } from "vee-validate/dist/rules";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// Override the default message.
+extend("required", {
+  ...required,
+  message: "This field is required",
+});
+
+const dateValidator = {
+  message: (field, args) => {
+    if (args[0] === "year") {
+      return "Must be in the past";
+    }
+    return `Must be a valid ${field}`;
+  },
+  validate(value, args) {
+    switch (args[0]) {
+      case "day":
+        return value >= 1 && value <= 31;
+      case "month":
+        return value >= 1 && value <= 12;
+      case "year":
+        return value <= 2023;
+    }
+    return false;
+  },
+};
+
+extend("date", dateValidator);
+
+setInteractionMode("eager");
+
+Vue.component("ValidationProvider", ValidationProvider);
+Vue.component("ValidationObserver", ValidationObserver);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
